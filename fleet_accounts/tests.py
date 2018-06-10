c1ff6ffa0fe5a46294feff2366f4d64cc5d936bf
Abdullahinur <aabdullahinur@hotmail.com>
2018-06-10 14:55:31 +0300

    made changes to owner model

--- fleet_accounts/tests.py
+++ fleet_accounts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
